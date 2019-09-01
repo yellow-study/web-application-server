fe9756c467981c769cef16b958e9c2e96225a1e8
윤주형 <juhyung0818@naver.com>
2019-09-01 17:24:19 +0900

    3.4.3.1 요구사항 1 - index.html 응답하기
    - StringUtils, lombok추가
    - index.html 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,24 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+
+    private static final int METHOD = 0;
+    private static final int URL = 1;
+    private static final int HTTP_VERSION = 2;
+
+    private static final String SPACE = StringUtils.SPACE;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -22,15 +28,48 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) {
+
+            String line = bufferedReader.readLine();
+            String requestLine = line;
+
+            while (StringUtils.isNotBlank(line)) {
+                line = bufferedReader.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            requestMapping(requestLine, dos);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void requestMapping(String line, DataOutputStream dos) throws IOException {
+        String[] requestLine = line.split(SPACE);
+        String url = requestLine[URL];
+
+        if (StringUtils.equals(requestLine[URL], "/index.html")) {
+            log.info("URL : {}", requestLine[URL]);
+            byte[] bytes = Files.readAllBytes(viewResolver(url));
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
+
+        } else {
+            byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        }
+
+    }
+
+    private Path viewResolver(String url) {
+        if (StringUtils.equals(url, "/")) {
+            return new File("./webapp/index.html").toPath();
+        }
+        return new File("./webapp" + url).toPath();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
