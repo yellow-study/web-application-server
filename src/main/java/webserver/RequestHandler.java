232577be8a8d79f9a399f98f4f59bf16fd854c42
sunyoung-dev <sunyoung-dev@gmail.com>
2019-09-17 21:50:33 +0900

    3.4.3.1 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -40,13 +44,27 @@ public class RequestHandler extends Thread {
                 line = br.readLine();
             }
 
+            byte[] body = getBody(url);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private byte[] getBody(String url) throws IOException {
+        if (url.startsWith("/user/create")) {
+            Map<String, String> queryMap = HttpRequestUtils.parseQueryString(url.split("\\?")[1]);
+            new User(queryMap.get("userId"),
+                    queryMap.get("name"),
+                    queryMap.get("password"),
+                    queryMap.get("name"));
+            return (queryMap.get("userId") + "님 환영합니다").getBytes();
+        }
+
+        return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
